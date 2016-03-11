f6a4b09d1c94e25dea9f464881a586d499b6f3fc
ehot_rascal <email@erscl.org>
2016-03-11 17:08:03 +0900

    FIX UnicodeDecodeError

--- lwp/__init__.py
+++ lwp/__init__.py
@@ -70,7 +70,7 @@ cgroup['allow'] = 'lxc.cgroup.devices.allow'
 
 
 def FakeSection(fp):
-    content = u"[DEFAULT]\n%s" % fp.read()
+    content = u"[DEFAULT]\n%s" % unicode(fp.read(), default_encoding, 'ignore')
 
     return StringIO(content)
 
